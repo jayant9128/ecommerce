e5b7de6c1894dc950630bf978d1dff3fca6325eb
jayant9128 <jayantkumar9128@gmail.com>
2024-07-29 10:50:54 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+
+
+route::get('/',[HomeController::class, 'home']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+route::get('admin/dashboard', [HomeController::class, 'index'])->middleware(['auth','admin']);
+route::get('view_category', [AdminController::class, 'view_category'])->middleware(['auth','admin']);
